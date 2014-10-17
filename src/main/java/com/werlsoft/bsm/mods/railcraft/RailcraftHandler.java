c5562d143ded08f902f4c7d7804a6f51a7c5242e
werl2 <peter@werl.me>
2014-10-16 22:03:01 -0600

    Update .gitignore, forge, TConstruct, Mantle versions. Added railcraft start

---
+++ src/main/java/com/werlsoft/bsm/mods/railcraft/RailcraftHandler.java
@@ -0,0 +1,9 @@
+package com.werlsoft.bsm.mods.railcraft;
+
+public class RailcraftHandler {
+	
+	public static void init(){
+		
+	}
+
+}
---
+++ src/main/java/com/werlsoft/bsm/mods/railcraft/blocks/SchematicRCStairs.java
@@ -0,0 +1,49 @@
+package com.werlsoft.bsm.mods.railcraft.blocks;
+
+import java.util.LinkedList;
+
+import net.minecraft.item.ItemStack;
+import buildcraft.api.blueprints.IBuilderContext;
+import buildcraft.api.blueprints.SchematicRegistry;
+import buildcraft.api.blueprints.SchematicTile;
+
+public class SchematicRCStairs extends SchematicTile {
+	@Override
+	public void writeRequirementsToWorld(IBuilderContext context, LinkedList<ItemStack> requirements) {
+		requirements.add(new ItemStack(block, 1, 0));
+	}
+
+	@Override
+	public void writeRequirementsToBlueprint(IBuilderContext context, int x, int y, int z) {
+
+	}
+
+	@Override
+	public boolean isAlreadyBuilt(IBuilderContext context, int x, int y, int z) {
+		return block == context.world().getBlock(x, y, z);
+	}
+
+	@Override
+	public void rotateLeft(IBuilderContext context) {
+		int pos = meta & 3;
+		int others = meta - pos;
+
+		switch (pos) {
+		case 0:
+			pos = 2;
+			break;
+		case 1:
+			pos = 3;
+			break;
+		case 2:
+			pos = 1;
+			break;
+		case 3:
+			pos = 0;
+			break;
+		}
+
+		meta = pos + others;
+	}
+	
+}
