8a2ec6b67032c8bfd67fe17a904ab23e475993eb
werl2 <peter@werl.me>
2014-10-18 22:45:09 -0600

    Started IC2 support

---
+++ src/main/java/com/werlsoft/bsm/mods/ic2/IC2Handler.java
@@ -0,0 +1,9 @@
+package com.werlsoft.bsm.mods.ic2;
+
+public class IC2Handler {
+	
+	public static void init(){
+		
+	}
+
+}
---
+++ src/main/java/com/werlsoft/bsm/mods/ic2/blocks/SchematicIC2EnergyStoreage.java
@@ -0,0 +1,23 @@
+package com.werlsoft.bsm.mods.ic2.blocks;
+
+import java.util.LinkedList;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import buildcraft.api.blueprints.IBuilderContext;
+
+public class SchematicIC2EnergyStoreage extends SchematicIC2Rotation {
+	
+	@Override
+	public void writeToWorld(IBuilderContext context, int x, int y, int z, LinkedList<ItemStack> stacks) {
+		if(this.tileNBT.hasKey("energy"))
+			this.tileNBT.setDouble("energy", 0);
+		if(this.tileNBT.hasKey("storage"))
+			this.tileNBT.setDouble("storage", 0);
+		if(this.tileNBT.hasKey("fuel"))
+			this.tileNBT.setInteger("fuel", 0);
+		if(this.tileNBT.hasKey("progress"))
+			this.tileNBT.setShort("progress", (short) 0);
+		super.writeToWorld(context, x, y, z, stacks);
+	}
+}
