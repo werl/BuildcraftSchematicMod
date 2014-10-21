78bd62d07b8ac4b8426aa92b4ebf3bd14c3c9dbc
werl2 <peter@werl.me>
2014-10-20 20:58:34 -0600

    kind of gotten forge multipart to work

--- src/main/java/com/werlsoft/bsm/mods/fmp/blocks/SchematicMultipart.java
+++ src/main/java/com/werlsoft/bsm/mods/fmp/blocks/SchematicMultipart.java
@@ -4,7 +4,12 @@ import java.util.LinkedList;
 
 import codechicken.multipart.TileMultipart;
 import codechicken.multipart.TileMultipartClient;
+import codechicken.multipart.asm.StackAnalyser.This;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.Packet;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.tileentity.TileEntity;
 import buildcraft.api.blueprints.IBuilderContext;
 import buildcraft.api.blueprints.SchematicTile;
@@ -16,24 +21,35 @@ public class SchematicMultipart extends SchematicTile {
 		// Meta needs to be specified twice, depending on the block behavior
 		context.world().setBlock(x, y, z, block, meta, 3);
 		context.world().setBlockMetadataWithNotify(x, y, z, meta, 3);
-		System.out.println("d");
 		
 		if (block.hasTileEntity(meta)) {
-			TileEntity tile = context.world().getTileEntity(x, y, z);
-
+			
 			tileNBT.setInteger("x", x);
 			tileNBT.setInteger("y", y);
 			tileNBT.setInteger("z", z);
 			
-			System.out.println("c");
-			if(tile != null){
-				System.out.println("a");
+			context.world().setTileEntity(x, y, z, TileMultipartClient.createFromNBT(tileNBT));
+			
+			TileEntity tile = context.world().getTileEntity(x, y, z);
+			
+			/*if(tile != null){
 				if(tile instanceof TileMultipart){
-					TileMultipart part = (TileMultipart) tile;
-					System.out.println("b");
-					context.world().setTileEntity(x, y, z, TileMultipartClient.createFromNBT(tileNBT));
+					TileMultipart part = (TileMultipart)tile;
+					for(int i = 0; i < context.world().playerEntities.size(); i++){
+						EntityPlayer player = (EntityPlayer) context.world().playerEntities.get(i);
+						if(player instanceof EntityPlayerMP){
+							System.out.println("a");
+							Packet packet = part.();
+
+				            if (packet != null)
+				            {
+				            	
+				                ((EntityPlayerMP)player).playerNetServerHandler.sendPacket(packet);
+				            }
+						}
+					}
 				}
-			}
+			}*/
 		}
 	}
 
