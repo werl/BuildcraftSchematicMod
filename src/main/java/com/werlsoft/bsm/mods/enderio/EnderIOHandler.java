a7b5f5d7c61942da7ffdc33da461a9e7fceb9f84
werl2 <peter@werl.me>
2014-10-23 22:36:27 -0600

    some work on backwards compatibility

--- src/main/java/com/werlsoft/bsm/mods/enderio/EnderIOHandler.java
+++ src/main/java/com/werlsoft/bsm/mods/enderio/EnderIOHandler.java
@@ -30,105 +30,167 @@ public class EnderIOHandler {
 		
 		try{
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockConduitBundle, SchematicConduitBundle.class);
-		} catch(Exception e){}
+		} catch(Exception e){
+			e.printStackTrace();
+		}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockStirlingGenerator, SchematicEIOEnergy.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockCombustionGenerator, SchematicEIOEnergy.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockZombieGenerator, SchematicZombieGenerator.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		
 		try {
 			//TODO get this working
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockReservoir, SchematicIgnore.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockAlloySmelter, SchematicEIOEnergy.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockCrusher, SchematicEIOEnergy.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		
 		try {
 			//TODO get this working
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockHyperCube, SchematicIgnore.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockPowerMonitor, SchematicEIOEnergy.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockVat, SchematicVat.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockFarmStation, SchematicEIOEnergy.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockTank, SchematicEIOTank.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockCrafter, SchematicEIOEnergy.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockPoweredSpawner, SchematicPoweredSpawner.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockSliceAndSplice, SchematicEIOEnergy.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockSoulFuser, SchematicEIOEnergy.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockAttractor, SchematicEIOEnergy.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockSpawnGuard, SchematicEIOEnergy.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockKillerJoe, SchematicKillerJoe.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockEnchanter, SchematicEIORotation.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		
 		try {
 			//TODO get this working
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockElectricLight, SchematicIgnore.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		
 		try {
 			//TODO get this working
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockLightNode, SchematicIgnore.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockPainter, SchematicEIOEnergy.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockPaintedFence, SchematicTileCustomStack.class, new ItemStack(Blocks.fence), paintEnergy);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockPaintedFenceGate, SchematicCustomRotateMeta.class, new int[]{0, 1, 2, 3}, true, new ItemStack(Blocks.fence_gate), paintEnergy);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockPaintedWall, SchematicTileCustomStack.class, new ItemStack(Blocks.cobblestone_wall), paintEnergy);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockPaintedStair, SchematicPaintedStairs.class);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockPaintedSlab, SchematicTileCustomStack.class, new ItemStack(Blocks.stone_slab), paintEnergy);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockPaintedDoubleSlab, SchematicTileCustomStack.class, new ItemStack(Blocks.stone_slab), paintEnergy);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockPaintedGlowstone, SchematicTileCustomStack.class, new ItemStack(Blocks.glowstone), paintEnergy);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 		try {
 			SchematicRegistry.registerSchematicBlock(EnderIO.blockPaintedCarpet, SchematicCustomPower.class, paintEnergy);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
 	}
 }
