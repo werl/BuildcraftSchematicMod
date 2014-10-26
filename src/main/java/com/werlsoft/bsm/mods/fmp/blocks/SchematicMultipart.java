75c208f5db44193fcaf3354bb9f66e244210032a
werl2 <peter@werl.me>
2014-10-25 18:29:41 -0600

    Multipart now renders properly on the client side, still no rotation

--- src/main/java/com/werlsoft/bsm/mods/fmp/blocks/SchematicMultipart.java
+++ src/main/java/com/werlsoft/bsm/mods/fmp/blocks/SchematicMultipart.java
@@ -2,6 +2,7 @@ package com.werlsoft.bsm.mods.fmp.blocks;
 
 import java.util.LinkedList;
 
+import codechicken.multipart.MultipartHelper;
 import codechicken.multipart.TileMultipart;
 import codechicken.multipart.TileMultipartClient;
 import codechicken.multipart.asm.StackAnalyser.This;
@@ -18,7 +19,6 @@ public class SchematicMultipart extends SchematicTile {
 	
 	@Override
 	public void writeToWorld(IBuilderContext context, int x, int y, int z, LinkedList<ItemStack> stacks) {
-		// Meta needs to be specified twice, depending on the block behavior
 		context.world().setBlock(x, y, z, block, meta, 3);
 		context.world().setBlockMetadataWithNotify(x, y, z, meta, 3);
 		
@@ -32,24 +32,12 @@ public class SchematicMultipart extends SchematicTile {
 			
 			TileEntity tile = context.world().getTileEntity(x, y, z);
 			
-			/*if(tile != null){
+			if(tile != null){
 				if(tile instanceof TileMultipart){
 					TileMultipart part = (TileMultipart)tile;
-					for(int i = 0; i < context.world().playerEntities.size(); i++){
-						EntityPlayer player = (EntityPlayer) context.world().playerEntities.get(i);
-						if(player instanceof EntityPlayerMP){
-							System.out.println("a");
-							Packet packet = part.();
-
-				            if (packet != null)
-				            {
-				            	
-				                ((EntityPlayerMP)player).playerNetServerHandler.sendPacket(packet);
-				            }
-						}
-					}
+					MultipartHelper.sendDescPacket(context.world(), part);
 				}
-			}*/
+			}
 		}
 	}
 
